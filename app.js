6eb20044b99f7e7c8cae8a847f2f242f89e418b2
User Undefined <zehiaelderthev@gmail.com>
2022-10-02 14:36:05 -0500

    Change config for assign a port automatic

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.get('/', function (req, resp) {
 	resp.send('Hello World')
 })
 
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!')
-})
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port '+ app.get('port'));
+});
