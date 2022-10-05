0faaa79a2c87cc4aeb7b778bc901fd8d5e36b625
User Undefined <zehiaelderthev@gmail.com>
2022-10-05 16:56:23 -0500

    Change clean code in app.js

--- app.js
+++ app.js
@@ -1,33 +1,26 @@
+
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const path = require('path') // Para acceder al directorio actual
+
 const app = express()
 
-const morgan = require('morgan');
-app.use(morgan('tiny'));
-
-const cors = require('cors');
-app.use(cors());
-
-app.use(express.json());
-
-// application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }))
-
-// Para acceder al directorio actual
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Middleware para Vue.js router modo history
-const history = require('connect-history-api-fallback');
-app.use(history());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), function () {
-  console.log('Example app listening on port '+ app.get('port'));
-});
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) // application/x-www-form-urlencoded
 
 app.get('/', (req, resp) => {
-  res.send('Hello World!');
+  resp.send('Hello World!')
 });
 
+// Middleware para vue.js router modo history
+const history = require('connect-history-api-fallback')
+app.use(history())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('port', process.env.PORT || 3000)
+app.listen(app.get('port'), () => {
+  console.log('Example app listening on port ' + app.get('port'))
+});
