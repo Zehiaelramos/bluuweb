fa04913567601325cb10375f2ae212f980a66220
User Undefined <zehiaelderthev@gmail.com>
2022-10-05 16:28:39 -0500

    Add history and static routes middlware config

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 app.use(express.json());
 
-//application/x-www-form-urlencoded
+// application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
 
 // Para acceder al directorio actual
@@ -17,11 +17,17 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, resp) {
-	resp.send('Hello World')
-})
+// Middleware para Vue.js router modo history
+const history = require('connect-history-api-fallback');
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port '+ app.get('port'));
 });
+
+app.get('/', (req, resp) => {
+  res.send('Hello World!');
+});
+
